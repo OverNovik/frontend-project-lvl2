04a28a077bb1e7a2aa3a73b29fdd7484f4cda241
Aliaxander Novikov <nerevarin11@gmail.com>
2022-02-28 13:52:10 +0300

    change option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --formatter <type>', 'output format', 'stylish')
   .arguments('filepath1 filepath2')
   .action((filepath1, filepath2, { formatter }) => {
     genDiff(filepath1, filepath2, formatter);
